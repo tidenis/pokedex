3f734e76d970ca4b429952e14ad797730d9e9792
Dênis Alves <startedenis@gmail.com>
2022-11-05 20:31:30 -0300

    Adição de ícone e arte de abertura

--- app/src/main/java/com/denis/alves/pokedex/ui/PokemonList/OpeningActivity.kt
+++ app/src/main/java/com/denis/alves/pokedex/ui/PokemonList/OpeningActivity.kt
@@ -5,14 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.denis.alves.pokedex.R
-import kotlinx.coroutines.android.asCoroutineDispatcher
 
 class OpeningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.opening_activity)
         startSecondActivity()
-
     }
 
     private fun startSecondActivity()
