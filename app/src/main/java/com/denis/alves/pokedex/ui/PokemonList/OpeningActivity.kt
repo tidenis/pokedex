15b4235b8e172d98469874c055b33b647b591961
Dênis Alves <startedenis@gmail.com>
2022-11-06 16:59:25 -0300

    cd_apk

--- app/src/main/java/com/denis/alves/pokedex/ui/PokemonList/OpeningActivity.kt
+++ app/src/main/java/com/denis/alves/pokedex/ui/PokemonList/OpeningActivity.kt
@@ -1,13 +1,20 @@
 package com.denis.alves.pokedex.ui.PokemonList
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.denis.alves.pokedex.R
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class OpeningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCenter.start(
+            application, "e1ea4645-4755-45f5-8b18-59ed940665e7",
+            Analytics::class.java, Crashes::class.java
+        )
         super.onCreate(savedInstanceState)
         setContentView(R.layout.opening_activity)
         startSecondActivity()
